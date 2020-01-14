c5274ce314d5694b1c4c095c04d1854c4f4597dd
InstagramModel <agd.mc1@gmail.com>
2020-01-14 16:41:06 -0600

    Update testing.js

--- testing.js
+++ testing.js
@@ -3,7 +3,7 @@ let split = string.split(' ')
 console.log(split);
 console.log(split.length);
 for (i=0;i<split.length;i++) {
-
+  
 }
 let letters = [
   'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
