b04c1b33d62fdeaf9f54e8ded7aaf5e7713b626c
InstagramModel <agd.mc1@gmail.com>
2020-01-18 11:37:35 -0600

    ShortestWord
    Fixed sorting after Index[0]

--- shortestWord.js
+++ shortestWord.js
@@ -4,6 +4,6 @@ let stringLengths = [];
 for (i=0;i<stringSplit.length;i++) {
 	stringLengths.push(stringSplit[i].length)
 }
-let sortedString = stringLengths.sort();
+let sortedString = stringLengths.sort((a,b) => a - b);
 let shortestLength = (sortedString[0]);
-console.log(shortestLength);
+console.log('The shortest word is ' + shortestLength + ' character(s) long');
